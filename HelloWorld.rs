19bbcef6d255f4a4066e73566fee0dffb45b1752
sashokpatil <sashokpatil@deloitte.com>
2024-03-18 15:40:18 +0530

    pushing first program

---
+++ HelloWorld.rs
@@ -0,0 +1,13 @@
+fn main(){
+    println!("Hello World");
+    println!("I'm a Rustacean!");
+    //println! is a macros which prints text to the console.
+    // RUST PROVIDES A MACRO SYSTEM WHICH ALLOWS METAPROGRAMMING -  
+    // METAPROGRAMMING - PROGRAMS CAN BE DESIGNED TO READ, TRANSFORM OTHER PROGRAMS N MODIFY ITSELF. 
+    // macros look like function except their name ends with !. 
+    // macros are expanded into source code that gets compiled with rest of the program.  
+}
+
+
+// rustc program.rs - to generate binary using Rust compiler.
+// ./program to execute the binary. 
